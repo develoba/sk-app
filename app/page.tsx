3ef1efd07b5aaf814a6e93c0a5b4a4bb70a8a186
develoba <srta.scobar@gmail.com>
2024-09-25 07:19:47 +0200

    feat: update main

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <main className="min-h-screen flex flex-col gap-8 items-center sm:items-start">
+      <main className="min-h-screen flex flex-col gap-8 items-center justify-center sm:items-start">
         <Link href="/routines">Enter</Link>
       </main>
     </div>
