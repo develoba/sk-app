c283791b38ea6115fe9a443f17e3704f7114ece0
develoba <srta.scobar@gmail.com>
2024-09-26 07:39:24 +0200

    feat: more config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 // next.config.js
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
 });
 
 /** @type {import('next').NextConfig} */
